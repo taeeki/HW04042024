11d3366b4194702dd934e95d623d8a40e0afd5bf
taeekii <terenteva1999@yandex.ru>
2024-04-14 13:35:32 +0300

    hw3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str = "teamwithsomeofexcersicesabcwanttomakeitbetter";
+if (str.Split("abc").Length > 1)
+{
+    string str1 = str.Split("abc")[0];
+    string str2 = str.Split("abc")[1];
+    Console.WriteLine($"{str1}\n{str2}");
+}
+else
+    Console.WriteLine("Разделить нельзя");

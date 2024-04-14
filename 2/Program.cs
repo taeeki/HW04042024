98aa73252e1e6f152079c9733981e59fe9e4d1e1
taeekii <terenteva1999@yandex.ru>
2024-04-14 13:33:21 +0300

    hw2

---
+++ 2/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] str = { "Welcome ", "to ", "the ", "TMS ", "lesons." };
+string concat_str = "";
+for (int i = 0; i < str.Length; i++)
+{
+    concat_str += string.Concat(str[i]);
+}
+Console.WriteLine(concat_str);

307898825c55627cdf9cb1d66b674a3b671543ff
taeekii <terenteva1999@yandex.ru>
2024-04-14 13:36:13 +0300

    hw4

---
+++ 4/Program.cs
@@ -0,0 +1,15 @@
+﻿string str = "Плохой день.";
+
+string substring = "Плохой";
+
+
+str = str.Substring(substring.Length);
+Console.WriteLine($"Результат удаления: {str}");
+
+str = str.Insert(0, "Хороший"); //Хороший день!!!!!!!!!
+str = str.Insert(str.Length - 1, "!!!!!!!!!");
+Console.WriteLine($"Результат добавления: {str}");
+
+str = str.Substring(0, str.Length - 1) + "?";
+
+Console.WriteLine($"Результат замены: {str}");
